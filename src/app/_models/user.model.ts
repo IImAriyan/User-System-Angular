3ce266ccf4fcbddb6fc739e1b464021c5436520c
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-12 22:24:59 +0330

    Getting User Service

--- src/app/_models/user.model.ts
+++ src/app/_models/user.model.ts
@@ -1,2 +1,10 @@
 export class User {
+  userID:number;
+  Username : string;
+  Password : string;
+  constructor(userID:number = 0 , Username: string = "", Password : string = "") {
+    this.userID = userID;
+    this.Username = Username;
+    this.Password = Password;
+  }
 }
--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {Observable} from "rxjs";
+import {User} from "../../_models/user.model";
+import {UserService} from "../../_services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  users$: User[] = [];
 
+  constructor(private userService: UserService, private router: Router) {
+    userService.getUsers().subscribe((response: User[]) => {
+      this.users$ = response;
+    })
+  }
 }
