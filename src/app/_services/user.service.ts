953af543b21bee70f7b1be9613a730642bf8d1fe
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-12 22:12:58 +0330

    Create User Model

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../_models/user.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  apiLink: string = "http://localhost:7740/api/";
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiLink + 'users-list')
+  }
 }
