a1e4328531de8f3408f072147296329f2d88e276
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-12 22:09:58 +0330

    Create User Service And Register Component And Set Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./_pages/home/home.component";
+import {RegisterComponent} from "./_pages/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'authentication/login',component:HomeComponent},
+  {path:'authentication/register',component:RegisterComponent},
+];
